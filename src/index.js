24ba3329b0e9767a3dc2d72d7a7c3d0572fa4a82
Xiao Fan <xiao.h.fan@gmail.com>
2017-10-08 14:05:56 -0700

    display clonability

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import xs from 'xstream'
 import {run} from '@cycle/run'
 import {div, span, makeDOMDriver} from '@cycle/dom'
 
+const hasPebble = (board, i, j) => board[i] && board[i][j]
+const clonable = (board, i, j) =>
+  hasPebble(board, i, j) &&
+  !hasPebble(board, i, j + 1) &&
+  !hasPebble(board, i + 1, j)
+
+const augmentedBoard = (board) =>
+  board.map((row, i) => row.map((pebble, j) =>
+    ({ pebble, clonable: clonable(board, i, j), location: [i, j] })
+  ))
+
+const displayBoard = (board) =>
+  div('.board', augmentedBoard(board).map(row =>
+    div('.row', row.map(({ pebble, clonable, location }) =>
+      span('.square.fa-circle', { class: { pebble, clonable } })
+    ))
+  ))
+
 const main = (sources) => {
   const board$ = xs.from(
     [[
@@ -9,13 +27,7 @@ const main = (sources) => {
       [true, false]
     ]])
 
-  const vdom$ = board$.map(
-    board => div('.board', board.map(
-      row => div('.row', row.map(
-        square => span('.square.fa-circle', { class: { pebble: square } })
-      ))
-    ))
-  )
+  const vdom$ = board$.map(displayBoard)
   return {
     DOM: vdom$
   }
