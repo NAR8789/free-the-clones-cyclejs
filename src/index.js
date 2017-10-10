0cfb8932a42d92cb8b725ae3b386e3ae0b7a0a46
Xiao Fan <xiao.h.fan@gmail.com>
2017-10-10 01:38:36 -0700

    rearrange to sandwich state management in the middle, because we can

--- src/index.js
+++ src/index.js
@@ -9,15 +9,16 @@ import { combinedDOM } from 'view'
 
 const main = (sources) => {
   const board1 = boardController.main1(sources)
+  const moveHistory1 = moveHistoryController.main1(board1)
+
   const board$ = board1.reducer$
     .fold((board, reducer) => reducer(board),
       [ [true, true],
         [true, false] ]
     )
-  const board2 = boardController.main2({ board$ })
-
-  const moveHistory1 = moveHistoryController.main1(board1)
   const moveHistory$ = moveHistory1.reducer$.fold((moveHistory, reducer) => reducer(moveHistory), [])
+
+  const board2 = boardController.main2({ board$ })
   const moveHistory2 = moveHistoryController.main2({ moveHistory$ })
 
   const combinedDOM$ = xs.combine(board2.DOM, moveHistory2.DOM)
