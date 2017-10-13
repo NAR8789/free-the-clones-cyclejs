918e4c461c73e66d47133252849bdd7a629d5f88
Xiao Fan <xiao.h.fan@gmail.com>
2017-10-13 00:48:51 -0700

    hack way of avoiding unnecessary container divs

--- src/undo-tree/view.js
+++ src/undo-tree/view.js
@@ -1,10 +1,10 @@
 import { div, button } from '@cycle/dom'
 
 export const withUndoRedo = (undoSelector, redoSelector) => (baseDOM) =>
-  div('.container', [
-    baseDOM,
-    div('#undo-controls', [
-      button(undoSelector, 'Undo'),
-      button(redoSelector, 'Redo'),
-    ])
-  ])
+  div('.container',
+    [ ...baseDOM,
+      div('#undo-controls', [
+        button(undoSelector, 'Undo'),
+        button(redoSelector, 'Redo'),
+      ]) ]
+  )
--- src/view.js
+++ src/view.js
@@ -1,7 +1,5 @@
 import { div } from '@cycle/dom'
 
 export const combinedDOM = ({ moveHistoryDOM, boardDOM }) =>
-  div('.container', [
-    div('#move-history', moveHistoryDOM),
-    div('#board', boardDOM)
-  ])
+  [ div('#move-history', moveHistoryDOM),
+    div('#board', boardDOM) ]
