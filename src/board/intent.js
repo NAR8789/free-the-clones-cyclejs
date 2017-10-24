eaae367203ac4072de0de295454c3e3a59a431a1
Xiao Fan <xiao@maven-labs.com>
2017-10-24 16:24:10 -0700

    since we end up needing to parse strings anyways, just use json

--- src/board/intent.js
+++ src/board/intent.js
@@ -1,2 +1,2 @@
 export const propagation = (propagationClick) =>
-  [parseInt(propagationClick.target.dataset.i), parseInt(propagationClick.target.dataset.j)]
+  JSON.parse(propagationClick.target.dataset.location)
--- src/board/view.js
+++ src/board/view.js
@@ -2,10 +2,10 @@ import { div, span } from '@cycle/dom'
 
 export const boardDOM = (boardPresenter) =>
   [ div('#board', div('.board', boardPresenter.map(row =>
-    div('.row', row.map(({ pebble, clonable, location: [i, j] }) =>
+    div('.row', row.map(({ pebble, clonable, location }) =>
       span('.square.fa-circle', {
         class: { pebble, clonable },
-        attrs: { 'data-i': i, 'data-j': j }
+        attrs: { 'data-location': JSON.stringify(location) }
       })
     ))
   ))) ]
