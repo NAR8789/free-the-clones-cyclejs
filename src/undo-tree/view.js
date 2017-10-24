ab228d3dbe5fe0bd73a68bb7a0dd8da5a1531669
Xiao Fan <xiao@maven-labs.com>
2017-10-24 15:14:57 -0700

    cruft cleanup

--- src/undo-tree/view.js
+++ src/undo-tree/view.js
@@ -5,10 +5,3 @@ export const undoControls = (undoSelector, redoSelector) => (undoTree) =>
     button(undoSelector, {attrs: {disabled: undoTree.undoDisabled}}, 'Undo'),
     button(redoSelector, {attrs: {disabled: undoTree.redoDisabled}}, 'Redo'),
   ])]
-
-export const withUndoRedo = (undoSelector, redoSelector) => (baseDOM) =>
-  [ ...baseDOM,
-    div('#undo-controls', [
-      button(undoSelector, {attrs: {disabled: 'true'}}, 'Undo'),
-      button(redoSelector, {attrs: {disabled: undefined}}, 'Redo'),
-    ]) ]
