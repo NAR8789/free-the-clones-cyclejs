b4ca92fd524e59da6190abdcaa1dc9242aae6706
Xiao Fan <xiao.h.fan@gmail.com>
2017-10-25 00:34:14 -0700

    shuffle around view logic

--- src/undo-tree/presenter.js
+++ src/undo-tree/presenter.js
@@ -3,14 +3,10 @@ import * as T from 'tree'
 export const undoTreePresenter = (undoTree) => {
   const undoable = T.hasParent(undoTree)
   const redoable = T.hasChildren(undoTree)
-  const undoDisabled = undoable ? false : 'true'
-  const redoDisabled = redoable ? false : 'true'
 
   return {
     ...undoTree,
     undoable,
     redoable,
-    undoDisabled,
-    redoDisabled,
   }
 }
--- src/undo-tree/view.js
+++ src/undo-tree/view.js
@@ -2,6 +2,6 @@ import { div, button } from '@cycle/dom'
 
 export const undoControls = (undoSelector, redoSelector) => (undoTree) =>
   [div('#undo-controls', [
-    button(undoSelector, {attrs: {disabled: undoTree.undoDisabled}}, 'Undo'),
-    button(redoSelector, {attrs: {disabled: undoTree.redoDisabled}}, 'Redo'),
+    button(undoSelector, {attrs: {disabled: undoTree.undoable ? false : 'true'}}, 'Undo'),
+    button(redoSelector, {attrs: {disabled: undoTree.redoable ? false : 'true'}}, 'Redo'),
   ])]
